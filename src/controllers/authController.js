32113b17a597c900ae3b2bef915b83a8240a5e59
Mahamodul Hasan Moon <mahamodulhasan.moon@gmail.com>
2023-07-03 18:00:16 +0600

    authentication system done

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,9 +2,11 @@ import { registerService } from "../services/auth.services.js";
 import { genarateToken } from "../utils/token.js";
 
 export const createUser = async (req, res, next) => {
+  console.log('from create user func',req.body)
+ 
     try {
       const userData = await registerService(req.body);
-      
+      console.log('form userData',req.body)
       const token = genarateToken(userData.email)
       res.status(200).json({
         status: "success",
